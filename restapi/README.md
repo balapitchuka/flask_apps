19a0a96931ac0d7323dea925b5358d9a52d18504
Balasubrahmanyam Pitchuka <pbs9948@gmail.com>
2020-09-13 00:08:25 +0530

    added readme for restapi

---
+++ restapi/README.md
@@ -0,0 +1,22 @@
+### Python Version
+```
+Python 3.6.9
+```
+
+### Install Dependencies
+```
+pip install -r requirements.txt
+```
+
+### Run Application
+```
+python simple_api.py
+```
+
+### Extensions For RestApi Development In Flask
+* [Flask-RESTful](https://flask-restful.readthedocs.io/en/latest/)
+
+* [Flask-RESTPlus](https://flask-restplus.readthedocs.io/en/stable/)
+
+### References
+* [CodeHandBook](https://codehandbook.org/)
--- restapi/simple_api.py
+++ restapi/simple_api.py
@@ -1,15 +1,26 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from flasgger import Swagger
+from functools import wraps
 
 app = Flask(__name__)
 
 # api documentation(refer /apidocs/)
 swagger = Swagger(app)
 
+def authorize(f):
+    @wraps(f)
+    def decorated_function():
+        key = request.args.get('api_key')
+        if key == 'abc123':
+            return f()
+        return jsonify({"statusCode":401, "message":"Un authorised access"})
+    return decorated_function
+
 """
 Api without using any extensions
 """
 @app.route('/employees', methods=['GET'])
+@authorize
 def get_all_employees():
     """
     endpoint returns list of employees
